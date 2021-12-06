6bcf80310981e2b5980e5382bd8d0a0610fa8333
kyaoNK <nkojima0823@gmail.com>
2021-12-07 00:15:26 +0900

    Remove pretty-print module in header file

--- parse.h
+++ parse.h
@@ -8,6 +8,9 @@
 /* Macro Syntax Result in Parse Module */
 #define NORMAL 0
 #define ERROR 1
+#define INTEGER 2
+#define BOOLEAN 3
+#define CHAR 4
 
 /* Macro Error Pattern in Parse Module */
 #define ERR_PAT_TOKEN 0
@@ -16,20 +19,11 @@
 /* Run Function in Parse */
 extern int parse ( void );
 
-
 /* Read Next Token and Pretty Print */
 extern void next_token ( void );
 
 /* Out Error Message */
 extern int error_syntax ( char * mes , int pattern );
-
-/* --- Pretty-Print Module Main Function --- */
-
-/* Init Pretty Print */
-extern void init_pretty ( void );
-
-/* Pretty Print */
-extern void pretty_print ( void );
 
 #endif /* PARSE_H_ */
 
